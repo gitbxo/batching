f0022f347abe919c99b5171181b0c9b9fcd562b7
Sanjay Khanna <sanjay.bxo@gmail.com>
2022-03-23 13:58:17 -0700

    improve test by using 5 batch types

--- example/src/main/java/org/bxo/example/ExampleBatch.java
+++ example/src/main/java/org/bxo/example/ExampleBatch.java
@@ -20,12 +20,18 @@ class ExampleBatch {
     public static void main(String[] args) throws InterruptedException {
 
         // These are the processors for (possibly different) batch types
-        StringBatch batch1 = new StringBatch("batch1", 7, 1000);
+        StringBatch batch1 = new StringBatch("batch1", 17, 1000);
         BatchTask.addBatch(batch1);
         // Here batch2 is the same processor as batch1, but it could
         // have been delete instead of insert, for example.
         StringBatch batch2 = new StringBatch("batch2", 11, 1000);
         BatchTask.addBatch(batch2);
+        StringBatch batch3 = new StringBatch("batch3", 7, 1000);
+        BatchTask.addBatch(batch3);
+        StringBatch batch4 = new StringBatch("batch4", 23, 1000);
+        BatchTask.addBatch(batch4);
+        StringBatch batch5 = new StringBatch("batch5", 19, 1000);
+        BatchTask.addBatch(batch5);
 
         // BatchTimer is required to start the background thread
         BatchTimer timer = new BatchTimer();
@@ -36,19 +42,36 @@ class ExampleBatch {
 
         for (int i=0; i < 100; i++) {
             for (String s : args) {
-                if (i%2 == 1) {
+                switch (i%5) {
+
+                case 1:
                     batch1.addItem(s);
-                } else {
+                    break;
+
+                case 2:
                     batch2.addItem(s);
+                    break;
+
+                case 3:
+                    batch3.addItem(s);
+                    break;
+
+                case 4:
+                    batch4.addItem(s);
+                    break;
+
+                default:
+                    batch5.addItem(s);
                 }
+
             }
             Thread.sleep(100);
         }
 
-	// This sleep is to wait till everything is written out
-	Thread.sleep(30_000);
-	// This stops the timer so that the program then exits
-	timer.stopTimer();
+        // This sleep is to wait till everything is written out
+        Thread.sleep(30_000);
+        // This stops the timer so that the program then exits
+        timer.stopTimer();
 
     }
 }
